5efa26826a4c1de01c0f56437ba755196ba1ad18
michiya0009 <javatea0009@gmail.com>
2015-08-25 06:33:32 +0000

    create destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,9 +17,30 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
-  ## ここまで
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
